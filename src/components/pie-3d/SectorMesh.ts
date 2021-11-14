abbb9208343c0d4dc8093581c76b9416ec8f105d
puxiao <yangpuxiao@gmail.com>
2021-11-15 00:33:23 +0800

    Update SectorMesh.ts

--- src/components/pie-3d/SectorMesh.ts
+++ src/components/pie-3d/SectorMesh.ts
@@ -1,18 +1,18 @@
-import { CylinderBufferGeometry, Material, Mesh, Object3D } from "three";
+import { Color, CylinderBufferGeometry, DoubleSide, Mesh, MeshBasicMaterial, MeshPhongMaterial, Object3D } from "three";
 import createSectorGeometry from "./createSectorGeometry";
 import RectangleGeometry from "./RectangleGeometry";
 
 //创建一个扇形3D对象
 class SectorMesh extends Object3D {
     private geometries: [CylinderBufferGeometry, RectangleGeometry, RectangleGeometry]
-    private _material: Material
+    private _color: Color
     private _height: number
 
-    constructor(radius: number, height: number, radialSegments: number, heightSegments: number, openEnded: boolean, thetaStart: number, thetaLength: number, material: Material) {
+    constructor(radius: number, height: number, radialSegments: number, heightSegments: number, openEnded: boolean, thetaStart: number, thetaLength: number, color: Color) {
         super()
         this.type = 'SectorMesh'
         this.geometries = createSectorGeometry(radius, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength)
-        this._material = material
+        this._color = color
         this._height = height
 
         this.update()
@@ -23,18 +23,24 @@ class SectorMesh extends Object3D {
             this.remove(this.children[0])
         }
 
-        this.geometries.forEach(geometry => {
-            this.add(new Mesh(geometry, this._material))
+        const cylinderMaterial = new MeshPhongMaterial({ color: this._color })
+        const sideMaterial = new MeshBasicMaterial({ color: this._color, side: DoubleSide })
+        this.geometries.forEach((geometry, index) => {
+            if (index === 0) {
+                this.add(new Mesh(geometry, cylinderMaterial))
+            } else {
+                this.add(new Mesh(geometry, sideMaterial))
+            }
         })
     }
 
-    public set material(value: Material) {
-        this._material = value
+    public set color(value: Color) {
+        this._color = value
         this.update()
     }
 
-    public get material(): Material {
-        return this._material
+    public get color(): Color {
+        return this._color
     }
 
     public get height(): number {
